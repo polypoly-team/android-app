262c2c9a7ab2f51ae5b2c69d18a4c844bdb58c65
victor <vrymergmd@gmail.com>
2023-03-02 22:43:38 +0100

    pretty start

--- app/src/main/java/com/github/vrooomer/polypoly/MainActivity.kt
+++ app/src/main/java/com/github/vrooomer/polypoly/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.github.vrooomer.polypoly
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,28 +17,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PolypolyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            Text("Hello World!")
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PolypolyTheme {
-        Greeting("Android")
     }
 }
