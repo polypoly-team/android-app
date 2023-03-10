3edb3cde867f7a6b4f5cdf3343f97a4da375c581
jadtl <03-grapple-very@icloud.com>
2023-03-10 15:11:29 +0100

    Optimize imports

--- app/src/main/java/com/github/polypoly/app/MapActivity.kt
+++ app/src/main/java/com/github/polypoly/app/MapActivity.kt
@@ -3,7 +3,6 @@ package com.github.polypoly.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
