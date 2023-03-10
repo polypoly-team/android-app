809dac6d30276949cf70e43ac2c02178010e8db4
Jharaxus <hugo.majerczyk@gmail.com>
2023-03-10 04:51:17 +0100

    Add basic game data structures

---
+++ app/src/main/java/com/github/polypoly/app/game/PendingGame.kt
@@ -0,0 +1,68 @@
+package com.github.polypoly.app.game
+
+import kotlin.time.Duration
+
+class PendingGame(
+    val admin: User,
+    val gameMode: GameMode,
+    val minimumNumberOfPlayers: Int,
+    val maximumNumberOfPlayers: Int,
+    val roundDuration: Duration,
+    val gameMap: List<Zone>,
+    val initialPlayerBalance: Int,
+    val name: String,
+    val code: String
+) {
+
+    private val currentUsersRegistered: ArrayList<User> = ArrayList()
+    val usersRegistered: List<User> get() = usersRegistered.toList()
+
+    init {
+        if (minimumNumberOfPlayers < 1)
+            throw java.lang.IllegalArgumentException("At least 2 players are needed for a game (provided $minimumNumberOfPlayers)")
+        if (maximumNumberOfPlayers < minimumNumberOfPlayers)
+            throw java.lang.IllegalArgumentException("Maximum number of players $maximumNumberOfPlayers must be greater than the minimum number $minimumNumberOfPlayers")
+        if (roundDuration.isNegative() || roundDuration.isInfinite())
+            throw java.lang.IllegalArgumentException("Invalid game duration$roundDuration")
+        if (name.isEmpty())
+            throw java.lang.IllegalArgumentException("Game name cannot be empty")
+        if (name.isBlank())
+            throw java.lang.IllegalArgumentException("Game name cannot be blank")
+        if (code.isEmpty())
+            throw java.lang.IllegalArgumentException("Game code cannot be empty")
+        if (code.isBlank())
+            throw java.lang.IllegalArgumentException("Game code cannot be blank")
+    }
+
+    fun addUser(user: User) {
+        if (currentUsersRegistered.size >= maximumNumberOfPlayers)
+            throw IllegalStateException("The game is already full")
+        if (currentUsersRegistered.any{u -> u.id == user.id})
+            throw java.lang.IllegalArgumentException("User $user is already registered")
+        currentUsersRegistered.add(user)
+    }
+
+    fun canStart(): Boolean {
+        return currentUsersRegistered.size in minimumNumberOfPlayers..maximumNumberOfPlayers
+    }
+
+    fun getRoundDuration(): Duration {
+        return roundDuration
+    }
+
+    fun removeUser(withId: Long) {
+        for (i in 0 until currentUsersRegistered.size) {
+            if (currentUsersRegistered[i].id == withId) {
+                currentUsersRegistered.removeAt(i)
+                return
+            }
+        }
+        throw java.lang.IllegalArgumentException("No user found with id $withId")
+    }
+
+    fun start(): Game {
+        if (!canStart())
+            throw java.lang.IllegalStateException("Try to start a game not ready to start yet")
+        return Game.launchFromPendingGame(this)
+    }
+}
