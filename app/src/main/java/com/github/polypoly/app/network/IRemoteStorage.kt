fc63a7d3a5f94e86f980cb009051704f2dc4f7ce
Jharaxus <hugo.majerczyk@gmail.com>
2023-03-17 01:56:13 +0100

    Improve IRemoteStorage interface

--- app/src/main/java/com/github/polypoly/app/network/IRemoteStorage.kt
+++ app/src/main/java/com/github/polypoly/app/network/IRemoteStorage.kt
@@ -4,5 +4,14 @@ import com.github.polypoly.app.game.User
 import java.util.concurrent.Future
 
 interface IRemoteStorage {
-    fun getUserWithId(userId: UInt): Future<User>
+    fun getUserProfileWithId(userId: Long): Future<User>
+
+    fun setUserProfileWithId(userId: Long, user: User): Future<User>
+
+    fun getAllUsers(): Future<List<User>>
+
+    /**
+     * Returns True if the user managed to be added
+     */
+    fun addUser(userId: Long): Future<Boolean>
 }
