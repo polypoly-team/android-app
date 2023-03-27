4ebcfbd419e85821848b99dc1a019a93d195da97
Maxime Teuber <maxime.teuber@epfl.ch>
2023-03-27 13:35:59 +0200

    implemented toString in GameMode for display purposes

--- app/src/main/java/com/github/polypoly/app/game/GameMode.kt
+++ app/src/main/java/com/github/polypoly/app/game/GameMode.kt
@@ -1,7 +1,13 @@
 package com.github.polypoly.app.game
 
-enum class GameMode(description: String) {
-    LAST_STANDING("In this game mode, the game lasts until only one player is left standing 😎"),
+enum class GameMode(val description: String, private val title: String) {
+    LAST_STANDING("In this game mode, the game lasts until only one player is left standing 😎", "Last Standing"),
     RICHEST_PLAYER("In this game mode, the game lasts for a fixed number of turn and the winner"
-        + "is the player with the greatest net worth in the end 🤑")
+        + "is the player with the greatest net worth in the end 🤑", "Richest Player");
+
+    override fun toString(): String {
+        return title
+    }
+
+
 }
