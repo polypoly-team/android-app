49092d7f6645e6ed3bb91b9ef45e39ad33499acf
jadtl <03-grapple-very@icloud.com>
2023-04-01 17:55:08 +0200

    Fix padding object

--- app/src/main/java/com/github/polypoly/app/utils/Padding.kt
+++ app/src/main/java/com/github/polypoly/app/utils/Padding.kt
@@ -2,10 +2,8 @@ package com.github.polypoly.app.utils
 
 import androidx.compose.ui.unit.dp
 
-class Padding {
-    companion object {
-        val small = 4.dp
-        val medium = 8.dp
-        val large = 16.dp
-    }
+object Padding {
+    val small = 4.dp
+    val medium = 8.dp
+    val large = 16.dp
 }
