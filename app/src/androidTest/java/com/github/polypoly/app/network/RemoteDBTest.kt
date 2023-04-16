afdd225879ad91e43e4a0260e2a530b7485192cc
Jharaxus <hugo.majerczyk@gmail.com>
2023-04-16 15:52:20 +0200

    Refactor RemoteStorage tests

--- app/src/androidTest/java/com/github/polypoly/app/network/RemoteDBTest.kt
+++ app/src/androidTest/java/com/github/polypoly/app/network/RemoteDBTest.kt
@@ -1,14 +1,14 @@
 package com.github.polypoly.app.network
 
-import com.github.polypoly.app.AppTest
 import com.github.polypoly.app.game.user.Skin
 import com.github.polypoly.app.game.user.Stats
 import com.github.polypoly.app.game.user.User
 import com.github.polypoly.app.global.GlobalInstances.Companion.remoteDB
-import com.github.polypoly.app.global.Settings.Companion.DB_USERS_PROFILES_PATH
 import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
@@ -18,38 +18,44 @@ import java.util.concurrent.ExecutionException
 import java.util.concurrent.TimeUnit
 
 @RunWith(JUnit4::class)
-class RemoteDBTest: AppTest() {
+class RemoteDBTest {
     companion object {
-        val TIMEOUT_DURATION = 1L
+        const val TIMEOUT_DURATION = 1L
+
+        val ZERO_STATS = Stats(0, 0, 0, 0, 0)
+        val NO_SKIN = Skin(0,0,0)
+
+        val TEST_USER_1 = User(1234L,"John", "Hi!", Skin(1, 1, 1),
+            ZERO_STATS, listOf(), mutableListOf())
+        val TEST_USER_2 = User(12345L,"Harry", "Ha!", Skin(1, 1, 1),
+            ZERO_STATS, listOf(), mutableListOf())
+        val TEST_USER_3 = User(123456L,"James", "Hey!", Skin(1, 1, 1),
+            ZERO_STATS, listOf(), mutableListOf())
+        val TEST_USER_4 = User(1234567L,"Henri", "Ohh!", Skin(1, 1, 1),
+            ZERO_STATS, listOf(), mutableListOf())
+
+        val TEST_ALL_USERS = listOf(TEST_USER_1, TEST_USER_2, TEST_USER_3, TEST_USER_4)
     }
 
     @Suppress("JoinDeclarationAndAssignment")
     private val rootRef: DatabaseReference
-    private val usersRootRef: DatabaseReference
-
-    private val zeroStats = Stats(0, 0, 0, 0, 0)
-
-    private val testUser1 = User(1234L,"John", "Hi!", Skin(1, 1, 1),
-        zeroStats, listOf(), mutableListOf())
-    private val testUser2 = User(12345L,"Harry", "Ha!", Skin(1, 1, 1),
-        zeroStats, listOf(), mutableListOf())
-    private val testUser3 = User(123456L,"James", "Hey!", Skin(1, 1, 1),
-        zeroStats, listOf(), mutableListOf())
-    private val testUser4 = User(1234567L,"Henri", "Ohh!", Skin(1, 1, 1),
-        zeroStats, listOf(), mutableListOf())
-    private val allTestUsers = listOf(testUser1, testUser2, testUser3, testUser4)
+    private val remoteStorageRootName = "test"
 
     init {
+        val db = Firebase.database
+        try {
+            db.setPersistenceEnabled(false)
+            remoteDB = RemoteDB(db, remoteStorageRootName)
+        } catch(_: java.lang.Exception) { }
         rootRef = remoteDB.rootRef
-        usersRootRef = rootRef.child(DB_USERS_PROFILES_PATH)
     }
 
-    fun addUsersToDB(users: List<User>) {
-        val timeouts = List(users.size) {CompletableFuture<Boolean>()}
+    private fun <T> addDataToDB(data: List<T>, keys: List<String>) {
+        val timeouts = List(data.size) {CompletableFuture<Boolean>()}
 
-        for (i in users.indices) {
-            val user = users[i]
-            usersRootRef.child(user.id.toString())
+        for (i in data.indices) {
+            val user = data[i]
+            rootRef.child(keys[i])
                 .setValue(user)
                 .addOnSuccessListener {
                     timeouts[i].complete(true)
@@ -59,9 +65,12 @@ class RemoteDBTest: AppTest() {
         timeouts.map{ timeout -> timeout.get(TIMEOUT_DURATION, TimeUnit.SECONDS)}
     }
 
-    fun removeAllUsersFromDB() {
+    private fun addUsersToDB(users: List<User>, root: String = "") = addDataToDB(users, users.map{user ->  root + user.id})
+
+    @Before
+    fun removeAllDataFromDB() {
         val timeout = CompletableFuture<Boolean>()
-        usersRootRef.removeValue()
+        rootRef.removeValue()
             .addOnSuccessListener {
                 timeout.complete(true)
             }.addOnFailureListener(timeout::completeExceptionally)
@@ -69,16 +78,16 @@ class RemoteDBTest: AppTest() {
     }
 
     @Test
-    fun userCanBeRetrievedFromId() {
-        addUsersToDB(listOf(testUser1))
-        val userFound = remoteDB.getValue<User>(DB_USERS_PROFILES_PATH + testUser1.id).get(TIMEOUT_DURATION, TimeUnit.SECONDS)
-        assertEquals(testUser1, userFound)
+    fun dataCanBeRetrievedFromKey() {
+        addUsersToDB(listOf(TEST_USER_1))
+        val userFound = remoteDB.getValue<User>(TEST_USER_1.id.toString()).get(TIMEOUT_DURATION, TimeUnit.SECONDS)
+        assertEquals(TEST_USER_1, userFound)
     }
 
     @Test
-    fun gettingUserOfInvalidIdFails() {
-        val invalidId = -1L
-        val failFuture = remoteDB.getValue<User>(DB_USERS_PROFILES_PATH + invalidId)
+    fun gettingDataWithInvalidKeyFails() {
+        val invalidKey = "invalid"
+        val failFuture = remoteDB.getValue<User>(invalidKey)
         failFuture.handle { _, exception ->
             assertTrue(exception != null)
             assertTrue(exception is IllegalAccessException)
@@ -87,39 +96,34 @@ class RemoteDBTest: AppTest() {
     }
 
     @Test
-    fun allUsersCanBeRetrievedAtOnce() {
-        removeAllUsersFromDB()
-        addUsersToDB(allTestUsers)
-
-        val allUsersFound = remoteDB.getAllValues<User>(DB_USERS_PROFILES_PATH).get(TIMEOUT_DURATION, TimeUnit.SECONDS)
-
-        assertEquals(allTestUsers.size, allUsersFound.size)
-        for (user in allTestUsers) {
-            assertTrue(allUsersFound.contains(user))
-        }
+    fun allDataWithSameKeyCanBeRetrievedAtOnce() {
+        val sharedKey = "all_users/"
+        addUsersToDB(TEST_ALL_USERS, sharedKey)
+        val allUsersFound = remoteDB.getAllValues<User>(sharedKey).get(TIMEOUT_DURATION, TimeUnit.SECONDS)
+        assertEquals(TEST_ALL_USERS.sortedBy(User::id), allUsersFound.sortedBy(User::id))
     }
 
     @Test
-    fun allUsersIdsCanBeRetrievedAtOnce() {
-        removeAllUsersFromDB()
-        addUsersToDB(allTestUsers)
+    fun allKeysCanBeRetrievedAtOnce() {
+        val rootKey = "root_key/"
+        val allUsersKeys = TEST_ALL_USERS.map{user -> user.id.toString()}
 
-        val allUsersIds = allTestUsers.map{user -> user.id.toString()}
-        val allUsersIdsFound = remoteDB.getAllKeys(DB_USERS_PROFILES_PATH).get(TIMEOUT_DURATION, TimeUnit.SECONDS)
-
-        assertEquals(allUsersIds.sorted(), allUsersIdsFound.sorted())
+        addUsersToDB(TEST_ALL_USERS, rootKey)
+        val allUsersKeysFound = remoteDB.getAllKeys(rootKey).get(TIMEOUT_DURATION, TimeUnit.SECONDS)
+        assertEquals(allUsersKeys.sorted(), allUsersKeysFound.sorted())
     }
 
     @Test
-    fun newUserCanBeRegistered() {
-        removeAllUsersFromDB()
-        assertTrue(remoteDB.registerValue(DB_USERS_PROFILES_PATH, testUser1).get(TIMEOUT_DURATION, TimeUnit.SECONDS))
+    fun newDataCanBeRegistered() {
+        assertTrue(
+            remoteDB.registerValue(TEST_USER_1.id.toString(), TEST_USER_1).get(TIMEOUT_DURATION, TimeUnit.SECONDS)
+        )
     }
 
     @Test
-    fun registeringAnAlreadyRegisteredUserFails() {
-        addUsersToDB(listOf(testUser1))
-        val failFuture = remoteDB.registerValue(DB_USERS_PROFILES_PATH, testUser1)
+    fun registeringAnAlreadyRegisteredDataFails() {
+        addUsersToDB(listOf(TEST_USER_1))
+        val failFuture = remoteDB.registerValue(TEST_USER_1.id.toString(), TEST_USER_1)
         failFuture.handle { _, exception ->
             assertTrue(exception != null)
             assertTrue(exception is IllegalAccessError)
@@ -128,21 +132,20 @@ class RemoteDBTest: AppTest() {
     }
 
     @Test
-    fun userCanBeUpdatedAfterRegistration() {
-        val userUpdated = User(testUser1.id, "Cool_name", "I updated my bio!", Skin(0,0,0),
-            zeroStats, listOf(), mutableListOf())
-        addUsersToDB(listOf(testUser1))
+    fun dataCanBeUpdatedAfterRegistration() {
+        val userUpdated = User(TEST_USER_1.id, "Cool_name", "I updated my bio!", NO_SKIN,
+            ZERO_STATS, listOf(), mutableListOf())
+        addUsersToDB(listOf(TEST_USER_1))
 
-        assertTrue(remoteDB.updateValue(DB_USERS_PROFILES_PATH + userUpdated.id, userUpdated).get(TIMEOUT_DURATION, TimeUnit.SECONDS))
+        assertTrue(remoteDB.updateValue(userUpdated.id.toString(), userUpdated).get(TIMEOUT_DURATION, TimeUnit.SECONDS))
 
-        val userUpdatedFound = remoteDB.getValue<User>(DB_USERS_PROFILES_PATH + userUpdated.id).get(TIMEOUT_DURATION, TimeUnit.SECONDS)
+        val userUpdatedFound = remoteDB.getValue<User>(userUpdated.id.toString()).get(TIMEOUT_DURATION, TimeUnit.SECONDS)
         assertEquals(userUpdated, userUpdatedFound)
     }
 
     @Test
-    fun unergisteredUserCannotBeUpdated() {
-        removeAllUsersFromDB()
-        val failFuture = remoteDB.updateValue(DB_USERS_PROFILES_PATH + testUser1.id, testUser1)
+    fun unergisteredDataCannotBeUpdated() {
+        val failFuture = remoteDB.updateValue(TEST_USER_1.id.toString(), TEST_USER_1)
         failFuture.handle { _, exception ->
             assertTrue(exception != null)
             assertTrue(exception is IllegalAccessError)
