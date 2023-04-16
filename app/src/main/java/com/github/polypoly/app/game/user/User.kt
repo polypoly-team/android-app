4550fcfe86441cf813239182bdcb5ed01ff17b71
tamaraaPrs <100276092+tamaraaPrs@users.noreply.github.com>
2023-04-16 16:12:43 +0200

    comment User

--- app/src/main/java/com/github/polypoly/app/game/user/User.kt
+++ app/src/main/java/com/github/polypoly/app/game/user/User.kt
@@ -4,18 +4,50 @@ package com.github.polypoly.app.game.user
  * Stub implementation of a User
  */
 data class User(
+    /**
+     * The id of the user, must be unique for each user
+     */
     val id: Long = 0,
+
+    /**
+     * The name of the user (can be personalized by the user)
+     */
     val name: String = "",
+
+    /**
+     * The bio of the user (can be personalized by the user)
+     */
     val bio: String = "",
+
+    /**
+     * The skin of the user (can be personalized by the user)
+     */
     val skin: Skin = Skin(),
+
+    /**
+     * The statistics about the user
+     */
     val stats: Stats = Stats(),
+
+    /**
+     * The list of the trophies won by the user
+     */
     val trophiesWon: List<Int> = listOf(),
+
+    /**
+     * The list of the trophies the user wants to display
+     */
     val trophiesDisplay: MutableList<Int> = mutableListOf(),
 ){
     override fun toString(): String {
         return "User{$id: $name}"
     }
 
+    /**
+     * Determine if the user has won the trophy
+     * @param trophyId the id of the trophy
+     * @return true if the user has won the trophy, false otherwise
+     */
     fun hasTrophy(trophyId: Int): Boolean {
         return trophiesWon.contains(trophyId)
     }
