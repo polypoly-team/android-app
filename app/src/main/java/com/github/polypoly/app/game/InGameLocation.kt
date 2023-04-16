0e545e0b523a45b04fd0c523281290051a132316
tamaraaPrs <100276092+tamaraaPrs@users.noreply.github.com>
2023-04-16 15:56:25 +0200

    Implement in game locations

---
+++ app/src/main/java/com/github/polypoly/app/game/InGameLocation.kt
@@ -0,0 +1,56 @@
+package com.github.polypoly.app.game
+
+/**
+ * A class that represent an location in a game
+ */
+data class InGameLocation (
+    /**
+     * The location of the in-game location, that permit to know the static info of the location
+     * as the name, the base price, the base tax price, etc...
+     */
+    var location: Location,
+
+    /**
+     * The current level of the location that determine the price and the tax price
+     */
+    var level: LocalizationLevel,
+
+    /**
+     * The owner of the location, if the location is not owned, the owner is null
+     */
+    var owner: Player?,
+) {
+
+    /**
+     * Tell if the location is owned by the player
+     * @param player the player to check
+     * @return true if the location is owned by the player, false otherwise
+     */
+    fun isTheOwner(player: Player): Boolean {
+        return owner?.user?.id == player.user.id
+    }
+
+    /**
+     * Compute the current tax of the location
+     * @return the current tax of the location
+     */
+    fun currentTax(): Int {
+        return location.baseTaxPrice * (level.ordinal+1)
+    }
+
+    /**
+     * Compute the current price of the location
+     * @return the current price of the location
+     */
+    fun currentPrice(): Int {
+        return location.basePrice * (level.ordinal+1)
+    }
+
+    /**
+     * Compute the current mortgage price of the location
+     * @return the current mortgage price of the location
+     */
+    fun currentMortgagePrice(): Int {
+        return location.baseMortgagePrice * (level.ordinal + 1)
+    }
+}
