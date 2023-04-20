987684957320806acb1f2a1be29306aa977311b0
tamaraaPrs <100276092+tamaraaPrs@users.noreply.github.com>
2023-04-20 16:12:34 +0200

    fix BonusCard magic number

--- app/src/main/java/com/github/polypoly/app/base/game/bonus_card/BonusCard.kt
+++ app/src/main/java/com/github/polypoly/app/base/game/bonus_card/BonusCard.kt
@@ -8,35 +8,28 @@ import com.github.polypoly.app.base.game.Player
  * @property title The title of the [BonusCard]
  * @property description The description of the [BonusCard]
  */
-data class BonusCard(
+enum class BonusCard(
     val id: Long,
     val title: String,
     val description: String,
 ) {
+
+    TEACHING_ASSISTANT_PAYDAY(0, title = "Teaching assistant payday", description = "You finally got your student assistant salary you've been waiting for after spending hours helping BA1s debug their code. Collect 100CHF!"),
+    TUITION_FEES_TO_PAY(1, title = "Tuition fees to pay!", description = "It's time to pay your tuition fees: you have passed the is academia deadline for far too long... Lose 100CHF.");
 
     /**
      * Apply the bonus of the [BonusCard] to the [Player]
      * @param player the [Player] to apply the bonus to
      */
     fun applyBonus(player: Player) {
-        when(id) {
-            0L -> {
+        when(this) {
+            TEACHING_ASSISTANT_PAYDAY -> {
                 player.earnMoney(100)
             }
-            1L -> {
+            TUITION_FEES_TO_PAY -> {
                 player.loseMoney(100)
             }
             // TODO : implement other bonus cards
         }
-    }
-
-    companion object {
-        /**
-         * The list of all [BonusCard]s possible to collect during a [Game]
-         */
-        val allBonusCards: List<BonusCard> = listOf(
-            BonusCard(0, title = "Teaching assistant payday", description = "You finally got your student assistant salary you've been waiting for after spending hours helping BA1s debug their code. Collect 100CHF!"),
-            BonusCard(1, title = "Tuition fees to pay!", description = "It's time to pay your tuition fees: you have passed the is academia deadline for far too long... Lose 100CHF."),
-        )
     }
 }
