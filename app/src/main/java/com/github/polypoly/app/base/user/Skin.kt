46dd1e39a5fb6f611ec17c1d10bd24c7e09f1f87
tamaraaPrs <100276092+tamaraaPrs@users.noreply.github.com>
2023-04-23 12:04:52 +0200

    fix codeclimate

--- app/src/main/java/com/github/polypoly/app/base/user/Skin.kt
+++ app/src/main/java/com/github/polypoly/app/base/user/Skin.kt
@@ -15,13 +15,14 @@ data class Skin(
     val idLegs: Int = 0
 ) {
     init {
+        fun testIdRange(id: Int, nb: Int) {
+            if (id < 0 || id >= nb)
+                throw java.lang.IllegalArgumentException("The id must be between 0 and ${nb-1}")
+        }
         // Checks that the skin exists
-        if (idHead < 0 || idHead >= NB_HEADS)
-            throw java.lang.IllegalArgumentException("The idHead must be between 0 and ${NB_HEADS-1}")
-        if (idBody < 0 || idBody >= NB_BODIES)
-            throw java.lang.IllegalArgumentException("The idBody must be between 0 and ${NB_BODIES-1}")
-        if (idLegs < 0 || idLegs >= NB_LEGS)
-            throw java.lang.IllegalArgumentException("The idLegs must be between 0 and ${NB_LEGS-1}")
+        testIdRange(idHead, NB_HEADS)
+        testIdRange(idBody, NB_BODIES)
+        testIdRange(idLegs, NB_LEGS)
     }
 
     companion object {
