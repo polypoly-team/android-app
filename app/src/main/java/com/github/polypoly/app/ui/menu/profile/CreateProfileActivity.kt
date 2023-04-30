7766f64b26030ddca0bf911848302041289a26c1
tamaraaPrs <100276092+tamaraaPrs@users.noreply.github.com>
2023-04-30 12:33:58 +0200

    button is disable when empty nickname

--- app/src/main/java/com/github/polypoly/app/ui/menu/profile/CreateProfileActivity.kt
+++ app/src/main/java/com/github/polypoly/app/ui/menu/profile/CreateProfileActivity.kt
@@ -3,22 +3,33 @@ package com.github.polypoly.app.ui.menu.profile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.github.polypoly.app.ui.theme.PolypolyTheme
+import com.github.polypoly.app.ui.theme.grey1
+import com.github.polypoly.app.ui.theme.grey2
 
+/**
+ * This activity is used to create a new profile for the user
+ * It is called when the the user registers for the first time or when he/she wants to play
+ * as a guest
+ */
 class CreateProfileActivity : ComponentActivity() {
 
     /**
      * The value of the nickname chosen by the user
      */
-    private var nickname: String = ""
+    private var nickname = mutableStateOf("")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,15 +46,28 @@ class CreateProfileActivity : ComponentActivity() {
     @Composable
     private fun CreateProfileContent() {
         Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-            modifier = Modifier.fillMaxSize()
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Top,
         ) {
-            Text(text = "Choose your nickname")
-            Spacer(modifier = Modifier.height(40.dp))
-            NicknameTextField()
-            Spacer(modifier = Modifier.height(40.dp))
-            ValidateButton()
+            Icon(
+                imageVector = Icons.Default.ArrowBack,
+                contentDescription = "Return Arrow",
+                tint = MaterialTheme.colors.onSecondary)
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Text(
+                    text = "How do you want\nto be named?",
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.h4,
+                )
+                Spacer(modifier = Modifier.height(60.dp))
+                NicknameTextField()
+                Spacer(modifier = Modifier.height(40.dp))
+                ValidateButton()
+            }
         }
     }
 
@@ -55,7 +79,7 @@ class CreateProfileActivity : ComponentActivity() {
         CustomTextField(
             label = "nickname",
             initialValue = "",
-            onChanged = {newValue ->  nickname = newValue},
+            onChanged = {newValue ->  nickname.value = newValue},
             maxTextLength = 15,
             testTag = "nicknameText",
         )
@@ -63,15 +87,22 @@ class CreateProfileActivity : ComponentActivity() {
 
     /**
      * The button to validate the profile creation
+     * It is disabled if the nickname is empty
      */
     @Composable
     private fun ValidateButton() {
+        remember { nickname }
         Button(
             onClick = {},
             shape = CircleShape,
             modifier = Modifier
                 .height(50.dp)
-                .width(300.dp)
+                .width(300.dp),
+            enabled = nickname.value.isNotEmpty(),
+            colors = ButtonDefaults.buttonColors(
+                disabledBackgroundColor = if (isSystemInDarkTheme()) grey2 else grey1,
+                disabledContentColor = if (isSystemInDarkTheme()) grey1 else grey2,
+            )
         ) {
             Text(text = "Let's go with this nickname!")
         }
