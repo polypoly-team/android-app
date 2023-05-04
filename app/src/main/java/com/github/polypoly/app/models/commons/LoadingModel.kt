a0bb6373f6fb72e943ea95560892c92ea7925e93
Jharaxus <hugo.majerczyk@gmail.com>
2023-05-04 08:50:38 +0200

    Add circular loader when GameLobby fetches the lobby selected

---
+++ app/src/main/java/com/github/polypoly/app/models/commons/LoadingModel.kt
@@ -0,0 +1,28 @@
+package com.github.polypoly.app.models.commons
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+/**
+ * Abstract model concept that can either be loading or not loading
+ */
+abstract class LoadingModel: ViewModel() {
+    private val isLoading: MutableLiveData<Boolean> = MutableLiveData(false)
+
+    /**
+     * Queries the loading status data
+     * @return a live data that is true iff the model is loading something
+     */
+    fun getIsLoading(): LiveData<Boolean> {
+        return isLoading
+    }
+
+    /**
+     * Updates the current loading state
+     * @param loading true iff the model is currently loading something
+     */
+    protected fun setLoading(loading: Boolean) {
+        isLoading.value = loading
+    }
+}
