cac666ae7d85b2b377d4e445957afedc717a1e6a
Jharaxus <hugo.majerczyk@gmail.com>
2023-05-24 22:54:00 +0200

    Add even more tests

--- app/src/androidTest/java/com/github/polypoly/app/base/game/GameTest.kt
+++ app/src/androidTest/java/com/github/polypoly/app/base/game/GameTest.kt
@@ -1,6 +1,8 @@
 package com.github.polypoly.app.base.game
 
 import com.github.polypoly.app.base.game.location.InGameLocation
+import com.github.polypoly.app.base.game.location.LocationBid
+import com.github.polypoly.app.base.game.location.LocationProperty
 import com.github.polypoly.app.base.game.location.LocationPropertyRepository
 import com.github.polypoly.app.base.menu.lobby.GameLobby
 import com.github.polypoly.app.base.menu.lobby.GameMode
@@ -8,6 +10,7 @@ import com.github.polypoly.app.base.menu.lobby.GameParameters
 import com.github.polypoly.app.base.user.Skin
 import com.github.polypoly.app.base.user.Stats
 import com.github.polypoly.app.base.user.User
+import com.github.polypoly.app.commons.PolyPolyTest
 import com.github.polypoly.app.ui.menu.lobby.GameLobbyConstants
 import com.github.polypoly.app.utils.global.GlobalInstances
 import org.junit.Assert.*
@@ -17,7 +20,7 @@ import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
 @RunWith(JUnit4::class)
-class GameTest {
+class GameTest: PolyPolyTest(false, false) {
 
     private val emptySkin = Skin(0,0,0)
     private val zeroStats = Stats(0,0,0,0,0)
@@ -144,6 +147,92 @@ class GameTest {
                         fail("Location $location is duplicated.")
                     ownedLocationsSet.add(location)
                 }
+        }
+    }
+
+    @Test
+    fun nextTurnAssignsLocationsCorrectly() {
+        val game = Game.launchFromPendingGame(TEST_GAME_LOBBY_FULL)
+
+        val player0 = game.getPlayer(TEST_USER_0.id)!!
+        val player1 = game.getPlayer(TEST_USER_1.id)!!
+        val player2 = game.getPlayer(TEST_USER_2.id)!!
+        val player3 = game.getPlayer(TEST_USER_3.id)!!
+        val player4 = game.getPlayer(TEST_USER_4.id)!!
+        val player5 = game.getPlayer(TEST_USER_5.id)!!
+
+        val location1 = getRandomLocation()
+        val location2 = getRandomLocation(excluding = listOf(location1))
+        val location3 = getRandomLocation(excluding = listOf(location1, location2))
+
+        game.registerBid(LocationBid(location1, player0, 300))
+        game.registerBid(LocationBid(location1, player1, 350))
+
+        game.registerBid(LocationBid(location2, player2, 500))
+        game.registerBid(LocationBid(location2, player3, 400))
+        game.registerBid(LocationBid(location2, player4, 450))
+
+        game.registerBid(LocationBid(location3, player5, 400))
+
+        game.nextTurn()
+
+        assertEquals(player1.getOwnedLocations().map(InGameLocation::locationProperty), listOf(location1))
+        assertEquals(player2.getOwnedLocations().map(InGameLocation::locationProperty), listOf(location2))
+        assertEquals(player5.getOwnedLocations().map(InGameLocation::locationProperty), listOf(location3))
+
+        assertTrue(player0.getOwnedLocations().isEmpty())
+        assertTrue(player3.getOwnedLocations().isEmpty())
+        assertTrue(player4.getOwnedLocations().isEmpty())
+    }
+
+    @Test
+    fun registerBidThrowsIfLocationIsNotPartOfGame() {
+        val game = Game.launchFromPendingGame(TEST_GAME_LOBBY_FULL)
+
+        val player0 = game.getPlayer(TEST_USER_0.id)!!
+        val location = LocationProperty(name = "I do not exist")
+
+        assertThrows(IllegalArgumentException::class.java) {
+            game.registerBid(LocationBid(location, player0, 350))
+        }
+    }
+
+    @Test
+    fun registerBidThrowsIfPlayerIsNotPartOfGame() {
+        val game = Game.launchFromPendingGame(TEST_GAME_LOBBY_FULL)
+
+        val player0 = Player(user = User(id = "I do not exist"))
+        val location = getRandomLocation()
+
+        assertThrows(IllegalArgumentException::class.java) {
+            game.registerBid(LocationBid(location, player0, 350))
+        }
+    }
+
+    @Test
+    fun registerBidThrowsIfPlayerAlreadyHasABid() {
+        val game = Game.launchFromPendingGame(TEST_GAME_LOBBY_FULL)
+
+        val player0 = game.getPlayer(TEST_USER_0.id)!!
+
+        val location1 = getRandomLocation()
+        val location2 = getRandomLocation(excluding = listOf(location1))
+
+        game.registerBid(LocationBid(location1, player0, 300))
+        assertThrows(IllegalStateException::class.java) {
+            game.registerBid(LocationBid(location2, player0, 350))
+        }
+    }
+
+    @Test
+    fun registerBidThrowsIfPlayerDoesNotHaveEnoughMoney() {
+        val game = Game.launchFromPendingGame(TEST_GAME_LOBBY_FULL)
+
+        val player0 = game.getPlayer(TEST_USER_0.id)!!
+        val location = getRandomLocation()
+
+        assertThrows(IllegalStateException::class.java) {
+            game.registerBid(LocationBid(location, player0, 10_000))
         }
     }
 }
