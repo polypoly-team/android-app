4a10147588b0390c7b937341561ce27624d6bfb4
tamaraaPrs <100276092+tamaraaPrs@users.noreply.github.com>
2023-05-25 23:01:53 +0200

    merge conflits

--- app/src/main/java/com/github/polypoly/app/ui/map/MapInitializationHelpers.kt
+++ app/src/main/java/com/github/polypoly/app/ui/map/MapInitializationHelpers.kt
@@ -4,15 +4,18 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.PorterDuff
 import android.graphics.PorterDuffColorFilter
+import android.graphics.RectF
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.location.Location
 import com.github.polypoly.app.R
 import com.github.polypoly.app.base.game.PlayerState
 import com.github.polypoly.app.base.game.location.LocationProperty
+import com.github.polypoly.app.base.game.location.PropertyLevel
 import com.github.polypoly.app.viewmodels.game.GameViewModel
 import org.osmdroid.tileprovider.MapTileProviderBasic
 import org.osmdroid.tileprovider.tilesource.OnlineTileSourceBase
@@ -31,6 +34,7 @@ import kotlin.random.Random
 private val INITIAL_POSITION = GeoPoint(46.518726, 6.566613)
 private const val INITIAL_ZOOM = 18.0
 private const val MARKER_SIDE_LENGTH = 100
+private const val STAR_SIDE_LENGTH = 50
 
 /**
  * Tile source for EPFL campus map.
@@ -77,22 +81,70 @@ fun initMapView(context: Context): MapView {
  * @param gameViewModel GameViewModel to use for game business logic. Null if no game is going on.
  * @return the marker that was added
  */
-fun addMarkerTo(mapView: MapView, location: LocationProperty, zoneColor: Int,
-                mapViewModel: MapViewModel, gameViewModel: GameViewModel?
+fun addMarkerTo(
+    mapView: MapView,
+    location: LocationProperty,
+    zoneColor: Int,
+    mapViewModel: MapViewModel,
+    gameViewModel: GameViewModel?,
+    propertyLevel: PropertyLevel,
+    owned: Boolean = true
 ): Marker {
-    fun buildMarkerIcon(context: Context, color: Int): Drawable {
+    fun buildMarkerIcon(
+        context: Context,
+        color: Int,
+        level: PropertyLevel,
+        isOwned: Boolean
+    ): Drawable {
         val markerIcon = BitmapFactory.decodeResource(context.resources, R.drawable.location_pin)
-        val scaledBitmap = Bitmap.createScaledBitmap(
-            markerIcon,
-            MARKER_SIDE_LENGTH,
-            MARKER_SIDE_LENGTH, true
-        )
+        val starIcon = BitmapFactory.decodeResource(context.resources, R.drawable.star_icon)
+        val scaledMarkerBitmap =
+            Bitmap.createScaledBitmap(markerIcon, MARKER_SIDE_LENGTH, MARKER_SIDE_LENGTH, true)
+        val scaledStarBitmap =
+            Bitmap.createScaledBitmap(starIcon, STAR_SIDE_LENGTH, STAR_SIDE_LENGTH, true)
+
         val paint = Paint()
         paint.colorFilter = PorterDuffColorFilter(color, PorterDuff.Mode.SRC_IN)
-        val canvas = Canvas(scaledBitmap)
-        canvas.drawBitmap(scaledBitmap, 0f, 0f, paint)
-        return BitmapDrawable(context.resources, scaledBitmap)
+
+        val strokeWidth = 5f // This is the width of the stroke. Adjust it as needed.
+        val strokePaint = Paint().apply {
+            style = Paint.Style.STROKE
+            this.color = Color.BLACK
+            this.strokeWidth = strokeWidth
+        }
+
+        val resultBitmap = Bitmap.createBitmap(
+            scaledMarkerBitmap.width,
+            scaledMarkerBitmap.height,
+            Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(resultBitmap)
+
+        // Draw the marker first
+        canvas.drawBitmap(scaledMarkerBitmap, 0f, 0f, paint)
+
+        // If the building is owned, draw the black border around the marker
+        if (isOwned) {
+            val rect = RectF(
+                strokeWidth / 2,
+                strokeWidth / 2,
+                scaledMarkerBitmap.width - strokeWidth / 2,
+                scaledMarkerBitmap.height - strokeWidth / 2
+            )
+            canvas.drawRect(rect, strokePaint)
+        }
+
+        // Draw the stars on top of the marker
+        for (i in 0 until level.ordinal) {
+            val left =
+                scaledMarkerBitmap.width / 2 - ((level.ordinal * STAR_SIDE_LENGTH) / 2) + i * STAR_SIDE_LENGTH
+            val top = (scaledMarkerBitmap.height - scaledStarBitmap.height) / 2.toFloat()
+            canvas.drawBitmap(scaledStarBitmap, left.toFloat(), top, null)
+        }
+
+        return BitmapDrawable(context.resources, resultBitmap)
     }
+
 
     val marker = Marker(mapView)
 
@@ -100,10 +152,11 @@ fun addMarkerTo(mapView: MapView, location: LocationProperty, zoneColor: Int,
     marker.title = location.name
     marker.isDraggable = false
     marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_CENTER)
-    marker.icon = buildMarkerIcon(mapView.context, zoneColor)
+    marker.icon = buildMarkerIcon(mapView.context, zoneColor, propertyLevel, owned)
 
     marker.setOnMarkerClickListener { _, _ ->
-        val interactionAllowed = gameViewModel?.getPlayerStateData()?.value == PlayerState.INTERACTING || gameViewModel == null
+        val interactionAllowed =
+            gameViewModel?.getPlayerStateData()?.value == PlayerState.INTERACTING || gameViewModel == null
         if (interactionAllowed && mapViewModel.getLocationSelected().value == null) {
             mapViewModel.selectLocation(location)
         }
@@ -115,13 +168,22 @@ fun addMarkerTo(mapView: MapView, location: LocationProperty, zoneColor: Int,
     return marker
 }
 
+fun removeMarkerFrom(mapView: MapView, location: LocationProperty) {
+    val marker = mapView.overlays.find { it is Marker && it.title == location.name } as Marker
+    mapView.overlays.remove(marker)
+}
+
 /**
  * Initializes the location overlay and sets the location listener.
  * @param mapViewModel GameViewModel to use for map business logic
  * @param gameViewModel GameViewModel to use for game business logic. Null if no game is going on
  * @return the location overlay that was added
  */
-fun initLocationOverlay(mapView: MapView, mapViewModel: MapViewModel, gameViewModel: GameViewModel?): MyLocationNewOverlay {
+fun initLocationOverlay(
+    mapView: MapView,
+    mapViewModel: MapViewModel,
+    gameViewModel: GameViewModel?
+): MyLocationNewOverlay {
     val locationProvider = GpsMyLocationProvider(mapView.context)
     var lastLocation = Location("")
 
@@ -135,12 +197,14 @@ fun initLocationOverlay(mapView: MapView, mapViewModel: MapViewModel, gameViewMo
     val locationOverlay = object : MyLocationNewOverlay(locationProvider, mapView) {
         override fun onLocationChanged(location: Location?, provider: IMyLocationProvider?) {
             super.onLocationChanged(location, provider)
-            gameViewModel?.computeClosestLocation(GeoPoint(location))?.thenApply(onClosestLocationFound)
+            gameViewModel?.computeClosestLocation(GeoPoint(location))
+                ?.thenApply(onClosestLocationFound)
             mapViewModel.addDistanceWalked(lastLocation.distanceTo(location!!))
             lastLocation = locationProvider.lastKnownLocation
             if (mapViewModel.currentPlayer != null
                 && gameViewModel?.getPlayerStateData()?.value == PlayerState.MOVING
-                && mapViewModel.interactableProperty.value == mapViewModel.goingToLocationProperty) {
+                && mapViewModel.interactableProperty.value == mapViewModel.goingToLocationProperty
+            ) {
                 gameViewModel.locationReached()
                 mapViewModel.goingToLocationProperty = null
             }
@@ -150,7 +214,8 @@ fun initLocationOverlay(mapView: MapView, mapViewModel: MapViewModel, gameViewMo
     locationOverlay.enableMyLocation()
     locationOverlay.enableFollowLocation()
     locationOverlay.runOnFirstFix {
-        gameViewModel?.computeClosestLocation(GeoPoint(locationOverlay.myLocation))?.thenApply(onClosestLocationFound)
+        gameViewModel?.computeClosestLocation(GeoPoint(locationOverlay.myLocation))
+            ?.thenApply(onClosestLocationFound)
         mapView.post {
             mapView.controller.animateTo(locationOverlay.myLocation)
         }
